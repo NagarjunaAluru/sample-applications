621b4b8d843f1765583b88916396f41a82991f93
NagarjunaAluru <54478184+NagarjunaAluru@users.noreply.github.com>
2019-09-11 15:44:08 +0530

    Add files via upload
    The initial version of Spring Boot MVC application

---
+++ simple-spring-mvc-rps/src/main/java/com/base/practice/SpringMvcRpsApplication.java
@@ -0,0 +1,17 @@
+package com.base.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * @author naluru
+ *
+ */
+@SpringBootApplication
+public class SpringMvcRpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcRpsApplication.class, args);
+	}
+}
